2582b8efc213e1fbd590a4333ac0c2b4f1991fbb
Andreas Boll Nielsen <andreas@boll-nielsen.dk>
2021-06-14 10:07:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BagageSorteringssystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
